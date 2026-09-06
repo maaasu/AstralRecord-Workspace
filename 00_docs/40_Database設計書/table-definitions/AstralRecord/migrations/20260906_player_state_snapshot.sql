1741ae8a967627e96f4b5ee5604011d21df6af76
maaasu <subetenokasei2022@gmail.com>
2026-09-07 00:14:02 +0900

    fix: legacy inventory item型を索引対応へ正規化

--- 00_docs/40_Database設計書/table-definitions/AstralRecord/migrations/20260906_player_state_snapshot.sql
+++ 00_docs/40_Database設計書/table-definitions/AstralRecord/migrations/20260906_player_state_snapshot.sql
@@ -6,6 +6,72 @@ IF EXISTS (SELECT 1 FROM sys.check_constraints
     WHERE [name] = N'CK_inventory_entry_payload'
       AND [parent_object_id] = OBJECT_ID(N'[dbo].[inventory_entry]'))
     ALTER TABLE [dbo].[inventory_entry] DROP CONSTRAINT [CK_inventory_entry_payload];
+
+-- Legacy databases may have item_id as NVARCHAR(MAX)/TEXT. Normalize it before
+-- creating the stack-item unique index; SQL Server does not allow those types
+-- as index key columns. Never truncate an existing item ID silently.
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns AS c
+    INNER JOIN sys.types AS t
+        ON t.user_type_id = c.user_type_id
+    WHERE c.[object_id] = OBJECT_ID(N'[dbo].[inventory_entry]')
+      AND c.[name] = N'item_id'
+      AND (
+          t.[name] IN (N'text', N'ntext', N'image')
+          OR (t.[name] IN (N'varchar', N'nvarchar', N'varbinary') AND c.[max_length] = -1)
+      )
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM sys.columns AS c
+        INNER JOIN sys.types AS t
+            ON t.user_type_id = c.user_type_id
+        WHERE c.[object_id] = OBJECT_ID(N'[dbo].[inventory_entry]')
+          AND c.[name] = N'item_id'
+          AND t.[name] = N'image'
+    )
+        THROW 51003, N'dbo.inventory_entry.item_id uses IMAGE. Convert it to a text type before applying this migration.', 1;
+
+    IF EXISTS (
+        SELECT 1
+        FROM [dbo].[inventory_entry]
+        WHERE [item_id] IS NOT NULL
+          AND DATALENGTH(CONVERT(NVARCHAR(MAX), [item_id])) > 200
+    )
+        THROW 51002, N'dbo.inventory_entry.item_id contains a value longer than NVARCHAR(100).', 1;
+
+    IF EXISTS (
+        SELECT 1
+        FROM sys.columns AS c
+        INNER JOIN sys.types AS t
+            ON t.user_type_id = c.user_type_id
+        WHERE c.[object_id] = OBJECT_ID(N'[dbo].[inventory_entry]')
+          AND c.[name] = N'item_id'
+          AND t.[name] IN (N'text', N'ntext', N'image')
+    )
+    BEGIN
+        EXEC sys.sp_executesql N'
+            ALTER TABLE [dbo].[inventory_entry]
+                ADD [item_id_migration] NVARCHAR(100) NULL;';
+        EXEC sys.sp_executesql N'
+            UPDATE [dbo].[inventory_entry]
+                SET [item_id_migration] = CONVERT(NVARCHAR(100), [item_id]);';
+        EXEC sys.sp_executesql N'
+            ALTER TABLE [dbo].[inventory_entry]
+                DROP COLUMN [item_id];';
+        EXEC sys.sp_executesql N'
+            EXEC sys.sp_rename N''dbo.inventory_entry.item_id_migration'', N''item_id'', N''COLUMN'';';
+    END;
+    ELSE
+    BEGIN
+        EXEC sys.sp_executesql N'
+            ALTER TABLE [dbo].[inventory_entry]
+                ALTER COLUMN [item_id] NVARCHAR(100) NULL;';
+    END;
+END;
+
 ALTER TABLE [dbo].[inventory_entry] WITH CHECK
     ADD CONSTRAINT [CK_inventory_entry_payload] CHECK (
         ([item_id] IS NOT NULL AND [instance_type] IS NULL AND [instance_id] IS NULL)
